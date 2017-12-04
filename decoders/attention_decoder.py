f5b101600cfddaabd2f05bdf21cbb49dba3ee34a
Baoguang Shi <shibaoguang@gmail.com>
2017-12-04 12:40:54 +0800

    WIP

--- decoders/attention_decoder.py
+++ decoders/attention_decoder.py
@@ -19,9 +19,13 @@ class AttentionDecoder():
     Args:
       feature_map: a float32 tensor with shape [batch_size, map_height, map_width, depth]
       last_state: a float32 tensor with shape [batch_size, ]
+      last_attention: a float32 tensor with shape [batch_size, map_height, map_width, depth]
     """
 
-    batch_size = 
+    batch_size = feature_map.get_shape()[0].value
+    feature_map_depth = feature_map.get_shape()[3].value
+    if batch_size is None or feature_map_depth is None:
+      raise ValueError('batch_size and feature_map_depth must be determined')
 
     vh = conv2d(
       feature_map,
@@ -37,33 +41,39 @@ class AttentionDecoder():
       stride=1,
       biases_initializer=None,
       reuse=reuse
-    ) # => [1, map_height, map_width, num_attention_units]
+    ) # => [batch_size, map_height, map_width, num_attention_units]
     ws = fully_connected(
       last_state,
       self._num_attention_units,
       activation_fn=None,
       biases_initializer=tf.zeros_initializer(),
-    ) # => [1, num_attention_units], bias is added
+    ) # => [batch_size, num_attention_units], bias is added
     attention_sum = tf.bias_add(
       tf.add(vh, last_attention_conv),
       ws
-    ) # => [1, map_height, map_width, num_attention_units]
+    ) # => [batch_size, map_height, map_width, num_attention_units]
     attention_scores = slim.conv2d(
       tf.tanh(att_sum),
       1,
       activation_fn=None,
       biases_initializer=None,
       reuse=reuse
-    ) # => [1, map_height, map_width, 1]
+    ) # => [batch_size, map_height, map_width, 1]
+    attention_scores_flat = tf.reshape(
+      tf.squeeze(attention_scores, axis=3),
+      [batch_size, -1, 1]
+    ) # => [batch_size, map_height * map_width, 1]
     attention_weights = tf.nn.softmax(
-      tf.reshape(tf.squeeze(attention_scores, axis=3),
-                 [batch_size, -1, 1]), # => [1, map_height * map_width, 1]
-      dim=1
-    ) # => [1, map_height * map_width, 1]
+      attention_scores_flat, dim=1
+    ) # => [batch_size, map_height * map_width, 1]
     feature_flat = tf.reshape(
       feature_map,
-      [num_heads, -1, feature_map_depth]
-    ) # [1, map_height * map_width, map_depth]
+      [batch_size, -1, feature_map_depth]
+    ) # => [batch_size, map_height * map_width, map_depth]
+    feature_flat_trans = tf.transpose(
+      feature_flat,
+      [0, 2, 1]
+    ) # => [batch_size, map_depth, map_height * map_width]
     glimpse = tf.squeeze(
       tf.matmul(feature_flat_trans, feature_flat),
       axis=2
