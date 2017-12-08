1d053ae7be54d967b8ad308402424798bc8d430b
Baoguang Shi <shibaoguang@gmail.com>
2017-12-08 16:45:05 +0800

    Label map

--- core/label_map.py
+++ core/label_map.py
@@ -1,5 +1,8 @@
 import string
 
+import tensorflow as tf
+
+
 class LabelMap(object):
 
   def __init__(self,
@@ -24,12 +27,24 @@ class LabelMap(object):
     return 0
 
   @property
+  def eos_char(self):
+    return ''
+
+  @property
   def go_label(self):
     return 1
 
   @property
+  def go_char(self):
+    return ''
+
+  @property
   def unk_label(self):
     return 2
+
+  @property
+  def unk_char(self):
+    return ''
 
   @property
   def num_control_symbols(self):
@@ -44,58 +59,68 @@ class LabelMap(object):
     return self.num_characters + self.num_control_symbols
 
   def _build_lookup_tables(self):
-    chars = ['', '', ''] + self._character_set
+    chars = [self.eos_char, self.go_char, self.unk_char] + self._character_set
     labels = [self.eos_label, self.go_label, self.unk_label] + \
              list(range(self.num_control_symbols, self.num_control_symbols + self.num_characters))
     char_to_label_table = tf.contrib.lookup.HashTable(
       tf.contrib.lookup.KeyValueTensorInitializer(
         chars[self.num_control_symbols:],
-        labels[self.num_control_symbols:]
+        labels[self.num_control_symbols:],
         key_dtype=tf.string,
-        value_dtype=tf.int32
-      )
+        value_dtype=tf.int64
+      ),
+      self.unk_label
     )
     label_to_char_table = tf.contrib.lookup.HashTable(
       tf.contrib.lookup.KeyValueTensorInitializer(
         labels,
         chars,
-        key_dtype=tf.int32,
+        key_dtype=tf.int64,
         value_dtype=tf.string
-      )
+      ),
+      self.unk_char
     )
     return char_to_label_table, label_to_char_table
 
   def text_to_labels(self, text):
     """Convert text strings to label sequences.
     Args:
-      text: ascii encoded string tensor of shape [batch_size]
-
+      text: ascii encoded string tensor with shape [batch_size]
+    Returns:
+      labels_padded: labels padded with eos symbols
     """
     batch_size = tf.shape(text)[0]
-    chars = tf.string_split(strings, delimiter='')
+    chars = tf.string_split(text, delimiter='')
     labels_sparse = self._char_to_label_table.lookup(chars.values)
-    labels_dense = tf.sparse_tensor_to_dense(
+    labels_dense = tf.sparse_to_dense(
       chars.indices,
       chars.dense_shape,
       labels_sparse,
-      default_value=self.eos_label
+      default_value=self.eos_label # use EOS to pad shorter text
     ) # => [batch_size, max_text_length]
 
     # pad eos symbols
+    eos_labels = tf.fill(
+      [batch_size, self._num_eos],
+      tf.constant(self.eos_label, dtype=tf.int64)
+    )
     labels_padded = tf.concat(
-      [labels_dense,
-       tf.fill([batch_size, self.num_eos])],
+      [labels_dense, eos_labels],
       axis=1
-    ) # => [batch_size, max_text_length + self.num_eos]
-
-    def _convert(text):
-      chars = tf.string_split(text, )
-      self._char_to_label_table.lookup()
-
+    ) # => [batch_size, max_text_length + self._num_eos]
+    return labels_padded
 
   def labels_to_text(self, labels):
     """Convert labels to text strings.
     Args:
-      labels: int32 tensor []
+      labels: int64 tensor with shape [batch_size, max_label_length]
+    Returns:
+      text: string tensor with shape [batch_size]
     """
-    pass
+    chars_sparse = self._label_to_char_table.lookup(labels)
+    chars_dense = tf.sparse_tensor_to_dense(
+      chars_sparse,
+      default_value=''
+    )
+    text = tf.reduce_join(chars_dense, axis=1)
+    return text
--- core/label_map_test.py
+++ core/label_map_test.py
@@ -1,3 +1,5 @@
+import string
+
 import tensorflow as tf
 
 from rare.core import label_map
@@ -6,14 +8,26 @@ from rare.core import label_map
 class LabelMapTest(tf.test.TestCase):
 
   def test_map_text_to_labels(self):
-    test_label_map = label_map.LabelMap(dictionary)
-
-    test_text = ["阿斯顿发", "asdf"]
-    test_text_tensor = tf.constant(
-      [t.encode('utf-8') for t in test_text],
-      tf.string)
+    with tf.device('/cpu:0'):
+      character_set = list(string.ascii_letters)
+      test_label_map = label_map.LabelMap(
+        character_set=character_set)
+      test_text = tf.constant(
+        ['a', 'b', '', 'abz', '0a='],
+        tf.string
+      )
+      test_labels = test_label_map.text_to_labels(test_text)
     with self.test_session() as sess:
-      print(test_text_tensor.eval())
+      tf.tables_initializer().run()
+      print(test_labels.eval())
+      self.assertAllEqual(
+        test_labels.eval(),
+        [[3, 0, 0, 0],
+         [4, 0, 0, 0],
+         [0, 0, 0, 0],
+         [3, 4, 28, 0],
+         [2, 3, 2, 0]]
+      )
 
 
 if __name__ == '__main__':
