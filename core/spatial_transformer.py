6c25d4e2d4e1caff0ccff114a3544bd6b0d751f6
Baoguang Shi <shibaoguang@gmail.com>
2018-01-06 15:46:19 +0800

    Spatial transformer works on batch

--- core/spatial_transformer.py
+++ core/spatial_transformer.py
@@ -4,6 +4,8 @@ import numpy as np
 from rare.utils import shape_utils
 
 
+eps = 1e-6
+
 class SpatialTransformer(object):
 
   def __init__(self,
@@ -36,7 +38,6 @@ class SpatialTransformer(object):
     # compute transformation
     C = tf.constant(self._output_ctrl_pts, tf.float32)
     Cp = input_ctrl_pts
-    n = input_ctrl_pts.shape[0]
     T = tf.matmul(tf.constant(self._inv_delta_c, dtype=tf.float32),
                   tf.concat([Cp, tf.zeros([3, 2])], axis=0))
 
@@ -49,13 +50,49 @@ class SpatialTransformer(object):
     C_tile = tf.expand_dims(C, axis=0) # => [1, k, 2]
     P_diff = P_tile - C_tile   # => [n, k, 2]
     P_norm = tf.norm(P_diff, axis=2, ord=2, keep_dims=False) # => [n, k]
-    eps = 1e-6
+
     rbf = tf.multiply(tf.square(P_norm), tf.log(P_norm + eps)) # => [n, k]
     P_lifted = tf.concat([ tf.ones([n, 1]), P, rbf ], axis=1)
     Pp = tf.matmul(P_lifted, T)
     return Pp
+
+  def _batch_generate_grid(self, input_ctrl_pts):
+    """
+    Args
+      input_ctrl_pts: float32 tensor of shape [batch_size, num_ctrl_pts, 2]
+    Returns
+      sampling_grid: float32 tensor of shape [num_sampling_pts, 2]
+    """
+    C = tf.constant(self._output_ctrl_pts, tf.float32) # => [k, 2]
+    batch_Cp = input_ctrl_pts # => [B, k, 2]
+    batch_size = input_ctrl_pts.shape[0]
+
+    inv_delta_c = tf.constant(self._inv_delta_c, dtype=tf.float32)
+    batch_inv_delta_c = tf.tile(
+      tf.expand_dims(inv_delta_c, 0),
+      [batch_size, 1, 1]) # => [B, k+3, k+3]
+    batch_Cp_zero = tf.concat(
+      [batch_Cp, tf.zeros([batch_size, 3, 2])],
+      axis=1) # => [B, k+3, 2]
+    batch_T = tf.matmul(batch_inv_delta_c, batch_Cp_zero) # => [B, k+3, 2]
+
+    k = self._num_control_points
+    G = tf.constant(self._output_grid.reshape([-1, 2]), tf.float32) # => [n, 2]
+    n = G.shape[0]
+    
+    G_tile = tf.tile(tf.expand_dims(G, axis=1), [1,k,1]) # => [n,k,2]
+    C_tile = tf.expand_dims(C, axis=0) # => [1, k, 2]
+    G_diff = G_tile - C_tile   # => [n, k, 2]
+    rbf_norm = tf.norm(G_diff, axis=2, ord=2, keep_dims=False) # => [n, k]
+    rbf = tf.multiply(tf.square(rbf_norm), tf.log(rbf_norm + eps)) # => [n, k]
+    G_lifted = tf.concat([ tf.ones([n, 1]), G, rbf ], axis=1) # => [n, k+3]
+    batch_G_lifted = tf.tile(tf.expand_dims(G_lifted, 0), [batch_size,1,1]) # => [B, n, k+3]
+
+    batch_Gp = tf.matmul(batch_G_lifted, batch_T)
+    return batch_Gp
   
   def _sample(self, image, sampling_grid):
+    sampling_grid = tf.maximum(0.0, tf.minimum(1.0-eps, sampling_grid))
     orig_dytpe = image.dtype
     image = tf.to_float(image)
     image_h, image_w, _ = shape_utils.combined_static_and_dynamic_shape(image)
@@ -88,10 +125,66 @@ class SpatialTransformer(object):
       tf.expand_dims(w11, axis=1) * I11,
     ])
     output_h, output_w = self._output_size
-    output_map = tf.reshape(pixels, [image_h, image_w, -1])
+    output_map = tf.reshape(pixels, [output_h, output_w, -1])
     output_map = tf.cast(output_map, dtype=orig_dytpe)
 
     return output_map
+
+  def _batch_sample(self, images, batch_sampling_grid):
+    """
+    Args:
+      images: tensor of any time with shape [batch_size, image_h, image_w, depth]
+      batch_sampling_grid; float32 tensor with shape [batch_size, num_sampling_pts, 2]
+    """
+    batch_G = tf.maximum(0.0, tf.minimum(1.0-eps, batch_sampling_grid)) # => [B, n, 2]
+    orig_dytpe = images.dtype
+    images = tf.to_float(images)
+    batch_size, image_h, image_w, _ = shape_utils.combined_static_and_dynamic_shape(images)
+    n = shape_utils.combined_static_and_dynamic_shape(batch_sampling_grid)[1]
+
+    batch_Gx = image_w * batch_G[:,:,0]
+    batch_Gy = image_h * batch_G[:,:,1]
+    batch_Gx0 = tf.cast(tf.floor(batch_Gx), tf.int32)
+    batch_Gx1 = batch_Gx0 + 1
+    batch_Gy0 = tf.cast(tf.floor(batch_Gy), tf.int32)
+    batch_Gy1 = batch_Gy0 + 1
+    # G* => [batch_size, n, 2]
+    # G*x, G*y => [batch_size, n]
+
+    def _get_pixels(images, batch_x, batch_y, batch_indices):
+      indices = tf.stack([batch_indices, batch_y, batch_x], axis=2) # => [B, n, 3]
+      pixels = tf.gather_nd(images, indices)
+      return pixels
+    
+    batch_indices = tf.tile(
+      tf.expand_dims(tf.range(batch_size), 1),
+      [1, n]) # => [B, n]
+    batch_I00 = _get_pixels(images, batch_Gx0, batch_Gy0, batch_indices)
+    batch_I01 = _get_pixels(images, batch_Gx0, batch_Gy1, batch_indices)
+    batch_I10 = _get_pixels(images, batch_Gx1, batch_Gy0, batch_indices)
+    batch_I11 = _get_pixels(images, batch_Gx1, batch_Gy1, batch_indices) # => [B, n, d]
+
+    batch_Gx0 = tf.to_float(batch_Gx0)
+    batch_Gx1 = tf.to_float(batch_Gx1)
+    batch_Gy0 = tf.to_float(batch_Gy0)
+    batch_Gy1 = tf.to_float(batch_Gy1)
+
+    batch_w00 = (batch_Gx1 - batch_Gx) * (batch_Gy1 - batch_Gy)
+    batch_w01 = (batch_Gx1 - batch_Gx) * (batch_Gy - batch_Gy0)
+    batch_w10 = (batch_Gx - batch_Gx0) * (batch_Gy1 - batch_Gy)
+    batch_w11 = (batch_Gx - batch_Gx0) * (batch_Gy - batch_Gy0) # => [B, n]
+
+    batch_pixels = tf.add_n([
+      tf.expand_dims(batch_w00, axis=2) * batch_I00,
+      tf.expand_dims(batch_w01, axis=2) * batch_I01,
+      tf.expand_dims(batch_w10, axis=2) * batch_I10,
+      tf.expand_dims(batch_w11, axis=2) * batch_I11,
+    ])
+
+    output_h, output_w = self._output_size
+    output_maps = tf.reshape(batch_pixels, [batch_size, output_h, output_w, -1])
+    output_maps = tf.cast(output_maps, dtype=orig_dytpe)
+    return output_maps
 
   def _build_output_grid(self):
     output_h, output_w = self._output_size
--- core/spatial_transformer_test.py
+++ core/spatial_transformer_test.py
@@ -34,26 +34,78 @@ class SpatialTransformerTest(tf.test.TestCase):
     output_grid = transformer._output_grid.reshape([-1, 2])
     output_ctrl_pts = transformer._output_ctrl_pts
 
-    # visualize
+    if False:
+      plt.figure()
+      plt.subplot(1,2,1)
+      plt.scatter(test_input_ctrl_pts[:,0], test_input_ctrl_pts[:,1])
+      plt.subplot(1,2,2)
+      plt.scatter(output_ctrl_pts[:,0], output_ctrl_pts[:,1])
 
-    plt.figure()
-    plt.subplot(1,2,1)
-    plt.scatter(test_input_ctrl_pts[:,0], test_input_ctrl_pts[:,1])
-    plt.subplot(1,2,2)
-    plt.scatter(output_ctrl_pts[:,0], output_ctrl_pts[:,1])
+      plt.figure()
+      plt.subplot(1,2,1)
+      plt.scatter(outputs['sampling_grid'][:,0], outputs['sampling_grid'][:,1])
+      plt.subplot(1,2,2)
+      plt.scatter(output_grid[:,0], output_grid[:,1])
 
-    plt.figure()
-    plt.subplot(1,2,1)
-    plt.scatter(outputs['sampling_grid'][:,0], outputs['sampling_grid'][:,1])
-    plt.subplot(1,2,2)
-    plt.scatter(output_grid[:,0], output_grid[:,1])
+      plt.figure()
+      plt.subplot(2,1,1)
+      plt.imshow(test_image_array)
+      plt.subplot(2,1,2)
+      plt.imshow(outputs['rectified_image'])
+      plt.show()
 
-    plt.figure()
-    plt.subplot(2,1,1)
-    plt.imshow(test_image_array)
-    plt.subplot(2,1,2)
-    plt.imshow(outputs['rectified_image'])
-    plt.show()
+  def test_batch_transform(self):
+    transformer = spatial_transformer.SpatialTransformer(
+      output_size=(32, 100),
+      num_control_points=6,
+      margin=0.05
+    )
+    test_input_ctrl_pts = np.array([
+      [
+        [0.1, 0.4], [0.5, 0.1], [0.9, 0.4],
+        [0.1, 0.9], [0.5, 0.6], [0.9, 0.9]
+      ],
+      [
+        [0.1, 0.1], [0.5, 0.4], [0.9, 0.1],
+        [0.1, 0.6], [0.5, 0.9], [0.9, 0.6]
+      ]
+    ], dtype=np.float32)
+    test_im = Image.open('rare/data/test_image.jpg').resize((100, 32))
+    test_image_array = np.array(test_im)
+    test_image_array = np.array([test_image_array, test_image_array])
+    test_images = tf.constant(test_image_array)
+    
+    sampling_grid = transformer._batch_generate_grid(test_input_ctrl_pts)
+    rectified_images = transformer._batch_sample(test_images, sampling_grid)
+
+    output_ctrl_pts = transformer._output_ctrl_pts
+    with self.test_session() as sess:
+      outputs = sess.run({
+        'sampling_grid': sampling_grid,
+        'rectified_images': rectified_images
+      })
+    
+    if True:
+      plt.figure()
+      plt.subplot(2,4,1)
+      plt.scatter(test_input_ctrl_pts[0,:,0], test_input_ctrl_pts[0,:,1])
+      plt.subplot(2,4,2)
+      plt.scatter(output_ctrl_pts[:,0], output_ctrl_pts[:,1])
+      plt.subplot(2,4,3)
+      plt.scatter(outputs['sampling_grid'][0,:,0], outputs['sampling_grid'][0,:,1])
+      plt.subplot(2,4,4)
+      plt.imshow(outputs['rectified_images'][0])
+
+      plt.subplot(2,4,5)
+      plt.scatter(test_input_ctrl_pts[1,:,0], test_input_ctrl_pts[1,:,1])
+      plt.subplot(2,4,6)
+      plt.scatter(output_ctrl_pts[:,0], output_ctrl_pts[:,1])
+      plt.subplot(2,4,7)
+      plt.scatter(outputs['sampling_grid'][1,:,0], outputs['sampling_grid'][1,:,1])
+      plt.subplot(2,4,8)
+      plt.imshow(outputs['rectified_images'][1])
+
+      plt.show()
 
 
 if __name__ == '__main__':
